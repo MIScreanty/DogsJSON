90b958227ddef1dfaf984e36e200e411ee4088a8
MIScreanty <redhatawka@gmail.com>
2024-10-17 13:44:08 +0300

    добавили progressbar

--- Gogs JSON.py
+++ Gogs JSON.py
@@ -1,17 +1,44 @@
-import requests
+import requests as r
 from tkinter import *
 from tkinter import ttk
 from PIL import Image, ImageTk
 from io import BytesIO
 
+def get_json_dog():
+    answer_api = r.get("https://dog.ceo/api/breeds/image/random")
+    json_dog = answer_api.json()
+    return json_dog["message"]
+
+def image_dog_in_tk():
+    url_image = get_json_dog()
+    if url_image:
+        answer_img = r.get(url_image)
+        image = BytesIO(answer_img.content)
+        img = Image.open(image)
+        img.thumbnail((500,350))
+        img_tk = ImageTk.PhotoImage(img)
+        i_m.config(image=img_tk)
+        i_m.image = img_tk
+    progress.stop()
+
+#Функция для отображения процесса загрузки
+def func_progress():
+    progress.config(value=0)
+    progress.start(25)
+    window.after(3000, image_dog_in_tk)
+
+
 window = Tk()
 window.title("Dogs JSON")
-window.geometry("500x400")
+window.geometry("500x450")
 
 i_m = Label(window)
-i_m.pack()
+i_m.pack(pady=[0,10])
 
-btn = Button(window, text="Get dog image")
-btn.pack()
+btn = ttk.Button(window, text="Get dog image", command=func_progress)
+btn.pack(pady=[0,10])
+
+progress = ttk.Progressbar(mode="determinate", length=400)
+progress.pack()
 
 window.mainloop()
