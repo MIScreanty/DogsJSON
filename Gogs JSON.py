3ec019572793538625c2d6e3378475ec55c608ed
MIScreanty <redhatawka@gmail.com>
2024-10-17 13:59:19 +0300

    добавили спинбокс для выбора высоты и ширины картинки добавили отображение картинки в новом окне

--- Gogs JSON.py
+++ Gogs JSON.py
@@ -15,10 +15,14 @@ def image_dog_in_tk():
         answer_img = r.get(url_image)
         image = BytesIO(answer_img.content)
         img = Image.open(image)
-        img.thumbnail((500,350))
+        img.thumbnail((int(spinbox_var_w.get()),int(spinbox_var_h.get())))
         img_tk = ImageTk.PhotoImage(img)
-        i_m.config(image=img_tk)
-        i_m.image = img_tk
+        new_win = Toplevel(window)
+        i_o = Label(new_win, image=img_tk)
+        i_o.image = img_tk
+        i_o.pack()
+        #i_m.config(image=img_tk)
+        #i_m.image = img_tk
     progress.stop()
 
 #Функция для отображения процесса загрузки
@@ -30,15 +34,27 @@ def func_progress():
 
 window = Tk()
 window.title("Dogs JSON")
-window.geometry("500x450")
+window.geometry("500x200")
 
-i_m = Label(window)
-i_m.pack(pady=[0,10])
+#i_m = Label(window)
+#i_m.pack(pady=[0,10])
 
 btn = ttk.Button(window, text="Get dog image", command=func_progress)
 btn.pack(pady=[0,10])
 
 progress = ttk.Progressbar(mode="determinate", length=400)
 progress.pack()
+
+spinbox_var_w = StringVar(value=250)
+spinbox_var_h = StringVar(value=250)
+
+width_m = Label(window, text="Width:")
+width_m.pack()
+width_img = ttk.Spinbox(window, from_=250, to=500, increment=50, textvariable=spinbox_var_w)
+width_img.pack()
+height_m = Label(window, text="Height:")
+height_m.pack()
+height_img = ttk.Spinbox(window, from_=250, to=500, increment=50, textvariable=spinbox_var_h)
+height_img.pack()
 
 window.mainloop()
